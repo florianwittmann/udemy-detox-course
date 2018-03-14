993788601ca1259d738a87a531fd0a03ea17071d
Florian Wittmann <dev@florianwittmann.de>
2018-03-14 16:38:01 +0100

    Passing of first test - expect an element

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
+        <Text testID={"welcome"} style={styles.welcome}>
           Welcome to React Native!
         </Text>
         <Text style={styles.instructions}>
